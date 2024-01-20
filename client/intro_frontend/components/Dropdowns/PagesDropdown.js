2a4352e7e4ab7735b2cd32e92183d1c34bcb3c43
Pratham Jagga <pratham.jagga@cloudeq.com>
2024-01-20 07:37:26 +0530

    ..

--- client/intro_frontend/components/Dropdowns/PagesDropdown.js
+++ client/intro_frontend/components/Dropdowns/PagesDropdown.js
@@ -125,7 +125,7 @@ const PagesDropdown = () => {
 							"text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
 						}
 					>
-						Profile
+						Community Profile
 					</a>
 				</Link>
 			</div>
--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	PORT: process.env.PORT || 3000,
 	MONGODB_URI:
-		process.env.MONGODB_URI || "mongodb://localhost:27017/open-source-hackfest",
+		process.env.MONGODB_URI ||
+		"mongodb+srv://pratham:pratham@cluster0.6b0ku.mongodb.net/?retryWrites=true&w=majority",
 };
