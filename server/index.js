51b5445d8d911ca320d1b082677cbbe71318483a
Adi2895 <jangraaditya11@gmail.com>
2024-01-20 08:03:57 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -27,3 +27,34 @@ mongoose
 
 app.use("/authenticate", authenticate)	
 app.use("/api/profiles", userRoutes);
+
+
+app.post("/auto-fix", async (req, res) => {
+	console.log(req.body);
+	try {
+		let script = `
+        cd /
+        ls
+        git clone https://${req.body.repoUrl}
+        cd ${req.body.repoName}
+        git checkout ${req.body.branch}
+        git pull
+        echo ${req.body.updatedCode} > ${req.body.filePath}
+        git add .
+        git commit -m "auto-fix: ${req.body.commitMessage}"
+        ls
+        git config --local credential.https://github.com.username ${req.body.githubUsername}
+        git config --local credential.https://github.com.password ${req.body.token}
+        git push origin ${req.body.branch}
+        cd ..
+        rm -rf ./${req.body.repoName}
+    `;
+		const scriptOutput = execSync(`powershell`, {
+			input: script,
+			encoding: "utf-8",
+		});
+		res.status(200).json({ success: true, output: scriptOutput });
+	} catch (err) {
+		res.status(500).json({ success: false, message: err });
+	}
+});
