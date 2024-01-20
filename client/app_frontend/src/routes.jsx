6a0333113acd2c938eaa53b42cf99e54b1f080c6
Adi2895 <jangraaditya11@gmail.com>
2024-01-20 07:53:27 +0530

    ..

--- client/app_frontend/src/routes.jsx
+++ client/app_frontend/src/routes.jsx
@@ -8,22 +8,13 @@ import {
   ComputerDesktopIcon,
   SunIcon,
   StarIcon,
- 
 } from "@heroicons/react/24/solid";
-import { Home, Profile, Tables, Notifications} from "@/pages/dashboard";
+import { Home, Profile, Tables, Notifications } from "@/pages/dashboard";
 import Matching from "@/pages/Matching";
 import Analyse from "@/pages/Analyse";
 import { SignIn, SignUp } from "@/pages/auth";
 import Intro from "@/Intro";
-<<<<<<< HEAD
-import ProjectView from "./pages/ProjectView";
-import ImageGrid from "./pages/GithubEcosystem";
-import AutoFix from "./pages/AutoFix";
 
-=======
-import Member from "./pages/dashboard/Members"
-import Projects from "./pages/dashboard/Project";
->>>>>>> a27f260 (..)
 const icon = {
   className: "w-5 h-5 text-inherit",
 };
@@ -35,31 +26,10 @@ export const routes = [
     pages: [
       {
         icon: <UserCircleIcon {...icon} />,
-        name: "Community Profile",
-        path: "/community-profile",
+        name: "profile",
+        path: "/profile",
         element: <Profile />,
       },
-      {
-<<<<<<< HEAD
-        name: "Project",
-        path: "/project",
-        element: <ProjectView />,
-      },
-
-=======
-        icon: <UserCircleIcon {...icon} />,
-        name: "project",
-        path: "/project",
-        element: <Projects/>,
-      },
-      {
-        icon: <UserCircleIcon {...icon} />,
-        name: "member",
-        path: "/member",
-        element: <Member />,
-      }
-      ,
->>>>>>> a27f260 (..)
       {
         icon: <SunIcon {...icon} />,
         name: "community connect",
@@ -84,16 +54,6 @@ export const routes = [
         name: "Your Github Stats",
         path: "/analyse",
         element: <Analyse />,
-      },
-      {
-        name: "Github Ecosystem Tools",
-        path: "/github-ecosystem",
-        element: <ImageGrid />,
-      },
-      {
-        name: "AutoFix",
-        path: "/autofix",
-        element: <AutoFix />,
       },
     ],
   },
